0b7653b9a1ad827980a2277a17e820f6150ad011
Vinay Bijalwan <vinaybijalwan@users.noreply.github.com>
2018-12-07 12:47:21 +0530

    this is first stage Feedback_Call project

---
+++ feedback/feedback_call/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Diagnosis, patientinfo
+
+class patientinfoForm(forms.ModelForm):
+
+    class Meta:
+        model = patientinfo
+        fields = '__all__'
+
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+    #     self.fields['Diagnosis'].queryset = Diagnosis.objects.none()
---
+++ feedback/feedback_call/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+    #path('', views.homePageView, name='home'),
+    #path('PatientListView', views.PatientListView, name='PatientListView'),
+    path('', views.pat_new, name='pat_new'),
+    #path('patient_edit', views.patient_edit, name='patient_edit'),
+    #path('thyroid', views.thyroid, name='thyroid'),
+]
